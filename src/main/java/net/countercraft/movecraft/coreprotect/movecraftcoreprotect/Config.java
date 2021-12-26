d62604737131e1d1f064b295b5f8a5e92b6ac65b
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-26 13:49:54 -0600

    Create Config.java

---
+++ src/main/java/net/countercraft/movecraft/coreprotect/movecraftcoreprotect/Config.java
@@ -0,0 +1,6 @@
+package net.countercraft.movecraft.coreprotect.movecraftcoreprotect;
+
+public class Config {
+    public static boolean LOG_INTERACTIONS = true;
+    public static boolean LOG_BLOCKS = true;
+}
