9cb6416c7f29686c2276dddbe103e98ef47106f7
TylerS1066 <TylerS1066@users.noreply.github.com>
2021-12-26 13:50:06 -0600

    Create MovecraftCoreProtect.java

---
+++ src/main/java/net/countercraft/movecraft/coreprotect/movecraftcoreprotect/MovecraftCoreProtect.java
@@ -0,0 +1,55 @@
+package net.countercraft.movecraft.coreprotect.movecraftcoreprotect;
+
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+import net.countercraft.movecraft.coreprotect.movecraftcoreprotect.listeners.CraftDetectListener;
+import net.countercraft.movecraft.coreprotect.movecraftcoreprotect.listeners.CraftReleaseListener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.Nullable;
+
+public final class MovecraftCoreProtect extends JavaPlugin {
+    @Nullable
+    private static MovecraftCoreProtect instance;
+
+    @Nullable
+    public static MovecraftCoreProtect getInstance() {
+        return instance;
+    }
+
+
+
+    @Nullable
+    private CoreProtectAPI coreProtectAPI;
+
+    @Override
+    public void onEnable() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+        if(!(plugin instanceof CoreProtect)) {
+            // error
+            coreProtectAPI = null;
+            setEnabled(false);
+            return;
+        }
+        coreProtectAPI = ((CoreProtect) plugin).getAPI();
+        if(!coreProtectAPI.isEnabled()) {
+            // error
+            coreProtectAPI = null;
+            setEnabled(false);
+            return;
+        }
+
+        getConfig().getBoolean("LogInteractions", true);
+        getConfig().getBoolean("LogBlocks", false);
+
+        getServer().getPluginManager().registerEvents(new CraftDetectListener(), this);
+        getServer().getPluginManager().registerEvents(new CraftReleaseListener(), this);
+
+        instance = this;
+    }
+
+    @Nullable
+    public CoreProtectAPI getCoreProtectAPI() {
+        return coreProtectAPI;
+    }
+}
