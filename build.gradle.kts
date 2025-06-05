1496128b0bb27a853f8d8cf84db6cfd31d6c2d17
TylerS1066 <TylerS1066@users.noreply.github.com>
2025-06-05 16:28:34 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("io.github.0ffz.github-packages") version "1.2.1"
+    id("io.github.apdevteam.github-packages") version "1.2.2"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
 }
 
